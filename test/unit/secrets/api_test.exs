ebc73616780bd2f583b29eb01b8a25fef5115721
gmile <iamexile@gmail.com>
2017-11-06 14:33:21 +0100

    Support swift backend for object storage

--- test/unit/secrets/api_test.exs
+++ test/unit/secrets/api_test.exs
@@ -8,7 +8,7 @@ defmodule Ael.Secrets.APITest do
     resource_id = "uuid"
     resource_name = "test.txt"
 
-    secret = create_secret("PUT", bucket, resource_id, resource_name, MIME.from_path(resource_name))
+    secret = create_secret("PUT", bucket, resource_id, resource_name, MIME.from_path(resource_name), "gcs")
 
     assert %Secret{
       action: "PUT",
@@ -35,13 +35,13 @@ defmodule Ael.Secrets.APITest do
 
     assert 200 == code
 
-    secret = create_secret("GET", bucket, resource_id, resource_name, "")
+    secret = create_secret("GET", bucket, resource_id, resource_name, "", "gcs")
 
     %HTTPoison.Response{body: body, status_code: code} = HTTPoison.get!(secret.secret_url)
     assert 200 == code
     assert File.read!(file_path) == body
 
-    secret = create_secret("HEAD", bucket, resource_id, resource_name, "")
+    secret = create_secret("HEAD", bucket, resource_id, resource_name, "", "gcs")
     %HTTPoison.Response{status_code: code} = HTTPoison.head!(secret.secret_url)
     assert 200 == code
   end
@@ -51,7 +51,7 @@ defmodule Ael.Secrets.APITest do
     resource_id = "uuid"
     resource_name = "test.txt"
 
-    secret = create_secret("PUT", bucket, resource_id, resource_name, "")
+    secret = create_secret("PUT", bucket, resource_id, resource_name, "", "gcs")
 
     assert %Secret{
       action: "PUT",
@@ -78,19 +78,62 @@ defmodule Ael.Secrets.APITest do
 
     assert 200 == code
 
-    secret = create_secret("GET", bucket, resource_id, resource_name, "")
+    secret = create_secret("GET", bucket, resource_id, resource_name, "", "gcs")
 
     %HTTPoison.Response{body: body, status_code: code} = HTTPoison.get!(secret.secret_url)
     assert 200 == code
     assert File.read!(file_path) == body
 
-    secret = create_secret("HEAD", bucket, resource_id, resource_name, "")
+    secret = create_secret("HEAD", bucket, resource_id, resource_name, "", "gcs")
+    %HTTPoison.Response{status_code: code} = HTTPoison.head!(secret.secret_url)
+    assert 200 == code
+  end
+
+  test "signes url's with resource name (swift; no content-type)" do
+    bucket = "declarations-dev"
+    resource_id = "uuid"
+    resource_name = "test.txt"
+
+    secret = create_secret("PUT", bucket, resource_id, resource_name, "", "swift")
+
+    assert %Secret{
+      action: "PUT",
+      bucket: ^bucket,
+      expires_at: _,
+      inserted_at: _,
+      resource_id: ^resource_id,
+      resource_name: resource_name,
+      content_type: "",
+      secret_url: secret_url
+    } = secret
+
+    assert "https://object.os.cloud.de-novo.biz" <> _ = secret_url
+
+    file_path = "test/fixtures/secret.txt"
+
+    headers = [
+      {"Accept", "*/*"},
+      {"Connection", "close"},
+      {"Cache-Control", "no-cache"},
+      {"Content-Type", ""},
+    ]
+    %HTTPoison.Response{body: _, status_code: code} = HTTPoison.put!(secret.secret_url, {:file, file_path}, headers)
+
+    assert 201 == code
+
+    secret = create_secret("GET", bucket, resource_id, resource_name, "", "swift")
+
+    %HTTPoison.Response{body: body, status_code: code} = HTTPoison.get!(secret.secret_url)
+    assert 200 == code
+    assert File.read!(file_path) == body
+
+    secret = create_secret("HEAD", bucket, resource_id, resource_name, "", "swift")
     %HTTPoison.Response{status_code: code} = HTTPoison.head!(secret.secret_url)
     assert 200 == code
   end
 
   test "signes url's without resource name" do
-    secret = create_secret("PUT", "declarations-dev", "uuid")
+    secret = create_secret("PUT", "declarations-dev", "uuid", "gcs")
 
     assert %Secret{
       action: "PUT",
@@ -104,23 +147,37 @@ defmodule Ael.Secrets.APITest do
     assert "https://storage.googleapis.com/declarations-dev/uuid" <> _ = secret_url
   end
 
-  def create_secret(action, bucket, resource_id) do
+  test "swift link is generated correctly" do
+    secret = %Secret{
+      action: "PUT",
+      expires_at: "2017-11-08 11:21:32Z",
+      bucket: "test-container",
+      resource_id: "B09E9D3E-EDE1-4A51-8FA7-46B4E8035795",
+      resource_name: "some-file.png"
+    }
+
+    url_params = "?temp_url_sig=11090e588a184bad8eeb284cb493026c1caf40a6&temp_url_expires=1510140092"
+
+    assert String.ends_with?(API.put_secret_url(secret, "swift").secret_url, url_params)
+  end
+
+  def create_secret(action, bucket, resource_id, backend) do
     {:ok, secret} = API.create_secret(%{
       action: action,
       bucket: bucket,
       resource_id: resource_id
-    })
+    }, backend)
     secret
   end
 
-  def create_secret(action, bucket, resource_id, resource_name, content_type) do
+  def create_secret(action, bucket, resource_id, resource_name, content_type, backend) do
     {:ok, secret} = API.create_secret(%{
       action: action,
       bucket: bucket,
       resource_id: resource_id,
       resource_name: resource_name,
       content_type: content_type
-    })
+    }, backend)
     secret
   end
 end
