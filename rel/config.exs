6a77de7c55f660f4f39159f7d9a601baa3a2349e
gmile <iamexile@gmail.com>
2017-11-25 17:41:12 +0200

    New base container

--- rel/config.exs
+++ rel/config.exs
@@ -5,7 +5,7 @@ use Mix.Releases.Config,
 environment :default do
   set pre_start_hook: "bin/hooks/pre-start.sh"
   set dev_mode: false
-  set include_erts: false
+  set include_erts: true
   set include_src: false
   set overlays: [
     {:template, "rel/templates/vm.args.eex", "releases/<%= release_version %>/vm.args"}
